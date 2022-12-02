d5be043811a279d392baf2523bba0dfdb4be1fd4
matubara <matsubara.h@gmail.com>
2022-12-03 03:11:03 +0900

    修正
    DOCKER内の/etc/hostsにホストPC（WSL UBUNTU）のIPアドレスを登録する

--- set-hosts-for-docker.sh
+++ set-hosts-for-docker.sh
@@ -1,3 +1,6 @@
+# https://www.ninton.co.jp/archives/5847
+# https://zenn.dev/datsukan/articles/a479f3e98a70c4
+
 autoexec=0
 pushd ../
 HOME_DIR=`pwd`/
@@ -9,16 +12,16 @@ echo "TARGET=${TARGET}"
 echo "Add ip address to /etc/hosts file in ${TARGET}_php container to be able to debug using XDEBUG."
   if [ $autoexec -eq 0 ];then read -p "Press [Enter] key to move on to the next."; fi
 
+TEMP_SH="`ip route | awk 'NR==1 {print $3}'` host.docker.internal"
+echo 'echo "'${TEMP_SH}'" >>/etc/hosts' > ./initialize-hosts.sh
+cat ./initialize-hosts.sh
+#exit
 #hostsファイルにIPアドレスを追加するためのスクリプト
-docker cp ${HOME_DIR}backup/initialize-hosts.sh \
+docker cp ${HOME_DIR}${TARGET}/initialize-hosts.sh \
         ${TARGET}_php:/home/kusanagi/${TARGET}/initialize-hosts.sh
 
-#docker exec -it --user root ${TARGET}_php \
-#        /bin/sh -c \
-#        "cd /home/kusanagi/${TARGET} \
-#         && echo \"echo \$(ip route | awk 'NR==1 {print \$3}') host.docker.internal >>/etc/hosts #test \" > initialize-hosts.sh \
-#         && chmod 777 initialize-hosts.sh \
-#         && /bin/sh initialize-hosts.sh"
-
-
-
+docker exec -it --user root ${TARGET}_php \
+        /bin/sh -c \
+        "cd /home/kusanagi/${TARGET} \
+         && chmod 777 initialize-hosts.sh \
+         && /bin/sh initialize-hosts.sh"
